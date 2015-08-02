b58c1187b73ed5a3a7b6633a7dcd2eb474b51913
Zorian64 <fedoryga_zorik@mail.ru>
2015-08-02 15:27:51 +0300

    code

---
+++ src/main/java/org/zorian/dao/TeamDAO.java
@@ -0,0 +1,17 @@
+package org.zorian.dao;
+
+import java.util.List;
+
+import org.zorian.model.Team;
+
+public interface TeamDAO {
+	public void addTeam(Team team);
+
+	public void updateTeam(Team team);
+
+	public Team getTeam(int id);
+
+	public void deleteTeam(int id);
+
+	public List<Team> getTeams();
+}
